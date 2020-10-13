fc4a062bdf0494f03eab4399d627541cebaf7074
Trufanov Roman <pingvin1308@gmail.com>
2020-10-13 10:14:49 +0300

    init

---
+++ DPV.School.IntroToLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DPV.School.IntroToLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
